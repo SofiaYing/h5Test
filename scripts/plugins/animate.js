2dd15fe7fb5aac6cbbf92f3cdc0c3af3564e70b0
zhangyy.dz <zhangyyfangzheng@163.com>
2019-05-29 13:40:38 +0800

    animate v

--- scripts/plugins/animate.js
+++ scripts/plugins/animate.js
@@ -31,15 +31,19 @@
                 var animations = valueTemp.states[0].animations
 
                 $.each(animations, function(index, item) {
-                    if (item.type.charAt(item.type.length - 1) === '0') {
+                    var autoAnimationCount = 0
+                    var clickAnimationCount = 0
+                    if (item.type.charAt(item.type.length - 1) === '0' && autoAnimationCount === 0) {
                         //载入页面动画 绑定视窗监听事件
                         intersectionObserverAutoAnimation.observe(animationDiv[0])
-                    } else if (item.type.charAt(item.type.length - 1) === '1') {
+                        autoAnimationCount += 1
+                    } else if (item.type.charAt(item.type.length - 1) === '1' && clickAnimationCount === 0) {
                         //单击动画 绑定单击事件
                         $(animationDiv[0]).on('click', function() {
                             $(this).addClass('animated fadeOut ' + 'delay-' + item.playDelay + 's')
                         })
                         intersectionObserverClickAnimation.observe(animationDiv[0])
+                        clickAnimationCount += 1
                     }
                 })
             })
@@ -56,6 +60,17 @@
 
         };
 
+        (function($) {
+            $.fn.extend({
+                animateCss: function(animationName) {
+                    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                    $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+                        $(this).removeClass('animated ' + animationName);
+                    });
+                }
+            });
+        })(jQuery);
+
         // 监听视窗：进入可视窗口单击可触发动画
         var intersectionObserverClickAnimation = new IntersectionObserver((entries) => {
             entries.forEach((item) => {
@@ -67,19 +82,24 @@
             })
         });
 
+
+
         // 监听视窗:进入可视窗口自动触发动画
         var intersectionObserverAutoAnimation = new IntersectionObserver((entries) => {
             entries.forEach((item) => {
+                console.log(item)
                 if (item.isIntersecting) {
                     var inputNode = $(item.target).parent().children('input')
                     var value = animationDataProcess(inputNode)
                     console.log('inputValue', value)
-
-                    $(item.target).addClass('animated fadeInUpBig ' + 'delay-' + value.animations.playDelay + 's')
                     $(item.target).css({ "animatiton-duration": value.animations.playTime + 's', "-webkit-animation-duration": value.animations.playTime + 's' })
-                } else {
-                    $(item.target).removeClass('animated fadeIn')
+                        // $(item.target).addClass('animated slideInUp ' + 'delay-' + value.animations.playDelay + 's')
+                    $(item.target).animateCss('animated fadeInUpBig ' + 'delay-' + value.animations.playDelay + 's')
+
                 }
+                // else {
+                //     $(item.target).removeClass('animated fadeIn')
+                // }
             })
         });
 
